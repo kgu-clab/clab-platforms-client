b154c6d0e6dc4408e7b4481418d460cdf1ac5163
yummjin <cho4u4o@gmail.com>
2026-02-21 22:06:01 +0800

    ✨ (land) API 통신 계층 추상화 (client -> method -> handler -> baseType)

---
+++ apps/member/src/shared/config/api-base-types.ts
@@ -0,0 +1,14 @@
+/* API 문서 확인 후 실제 타입에 따라 정의 수정
+핸들러 함수에서 Type Parameter로 넘기는 BaseType
+ **/
+
+export type ApiResponse<T> = {
+  code: string;
+  message: string;
+  result: T;
+};
+
+export type ApiResponseWithoutResult = {
+  code: string;
+  message: string;
+};
