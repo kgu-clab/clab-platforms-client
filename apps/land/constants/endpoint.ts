c8274f89b83263bab7997dfcbd94eeba72499521
yummjin <cho4u4o@gmail.com>
2026-02-22 21:18:44 +0900

    ✨ (land) API URL 설정 및 리라이트 규칙 추가

--- apps/land/constants/endpoint.ts
+++ apps/land/constants/endpoint.ts
@@ -1,9 +1,10 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_BASE_URL =
+  typeof window !== 'undefined' && process.env.NODE_ENV === 'production'
+    ? '/api'
+    : process.env.NEXT_PUBLIC_API_URL;
 export const SERVER_BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 export const CHANNEL_TALK_TOKEN = process.env.NEXT_PUBLIC_CHANNEL_TALK_TOKEN;
-/**
- * API 엔드포인트를 정의합니다.
- */
+
 export const END_POINT = {
   RECRUITMENT: `/v1/recruitments`,
   RECRUITMENT_DETAIL: (recruitmentId: number) => `/v1/recruitments/${recruitmentId}`,
--- apps/land/next.config.ts
+++ apps/land/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
 
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${apiBaseUrl}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
