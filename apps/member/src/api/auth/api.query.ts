6e5812bcce709c8fdb39201b4ca848457488ea29
yummjin <cho4u4o@gmail.com>
2026-02-26 19:00:59 +0900

    ✨ (member) 로그인 모델 타입 및 핸들러 함수 정의

--- apps/member/src/api/auth/api.query.ts
+++ apps/member/src/api/auth/api.query.ts
@@ -1,1 +1,10 @@
-// 쿼리키
+import { mutationOptions } from "@tanstack/react-query";
+
+import type { PostLoginRequest } from "./api.model";
+import { postLogin } from "./postLogin";
+
+export const authQueries = {
+  postLoginMutation: mutationOptions({
+    mutationFn: ({ id, password }: PostLoginRequest) => postLogin(id, password),
+  }),
+};
--- apps/member/src/api/auth/postLogin.ts
+++ apps/member/src/api/auth/postLogin.ts
@@ -1,1 +1,11 @@
-// 로그인 api 핸들러 구현
+import { type ApiResponseWithoutResult, baseApi, END_POINT } from "../config";
+import type { PostLoginRequest } from "./api.model";
+
+export const postLogin = (id: string, password: string) =>
+  baseApi.post<ApiResponseWithoutResult, PostLoginRequest>(
+    END_POINT.AUTH.LOGIN,
+    {
+      id,
+      password,
+    },
+  );
