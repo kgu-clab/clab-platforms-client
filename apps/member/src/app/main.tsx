215eaaec92f99c6999c44ef0586d3f927e52bef1
seoJing <tjwlsrb1021@naver.com>
2026-03-03 23:42:01 +0900

    :sparkles: (member) sonner 설치 및 세팅
    sonner toaster를 세팅 및 디자인을 적용합니다.
    
    Closes

--- apps/member/src/app/main.tsx
+++ apps/member/src/app/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
+import { Toaster } from "sonner";
 
 import { router } from "./route/config";
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster />
     </QueryClientProvider>
   </StrictMode>,
 );
