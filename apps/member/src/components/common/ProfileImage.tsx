be56e4f0e139e95567b099485ad91b13ea76b81c
seoJing <tjwlsrb1021@naver.com>
2026-03-09 02:13:38 +0900

    :recycle: (member) 프로필 이미지에 에러 처리 추가
    Closes

--- apps/member/src/components/common/ProfileImage.tsx
+++ apps/member/src/components/common/ProfileImage.tsx
@@ -2,6 +2,7 @@ import { cn } from "@clab/design-system";
 import { IoPersonSharp } from "react-icons/io5";
 
 import { BASE_FILE_URL } from "@/api/config";
+import { useState } from "react";
 
 interface ProfileImageProps {
   imageUrl?: string;
@@ -14,13 +15,15 @@ export default function ProfileImage({
   size = "size-16",
   className,
 }: ProfileImageProps) {
+  const [imgError, setImgError] = useState(false);
   const fullUrl = imageUrl ? `${BASE_FILE_URL}${imageUrl}` : undefined;
 
-  return imageUrl ? (
+  return imageUrl && !imgError ? (
     <img
       src={fullUrl}
       alt="프로필"
       className={cn(size, "rounded-full object-cover", className)}
+      onError={() => setImgError(true)}
     />
   ) : (
     <div
